9d5c627b354c0c2d513243be9908f6ae27f308af
SirRicharod <riccardo.contact@proton.me>
2026-02-13 21:26:44 +0100

    Current date and time added, updated styling

--- index.php
+++ index.php
@@ -8,13 +8,19 @@
     <title>𝙋𝙊𝙈𝙊𝘿𝙊𝙍𝙊 🍅</title>
 
     <!-- Bootstrap CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
     <!-- Bootstrap Icons CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
     <!-- Stylesheet -->
     <link rel="stylesheet" href="style.css">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -22,9 +28,9 @@
         <div class="p-3 d-flex justify-content-between align-content-center">
             <div class="text-center">
                 <div class="date">
-                    fri 12 2025
+                    <?php echo date("D, d/m/Y") ?>
                 </div>
-                <div class="time">
+                <div class="time" id="time">
                     10:25:55
                 </div>
             </div>
@@ -75,8 +81,11 @@
     </section>
 
     <section class="d-flex justify-content-center align-content-center row text-center">
-        <div class="p-3">
-            The Task
+        <div class="p-3 task d-flex justify-content-center align-content-center row">
+            <div class="label">
+                The Task
+                <div class="underline"></div>
+            </div>
         </div>
         <div class="p-1">
             <input type="text" placeholder="What task should we focus on?" class="w-50">
@@ -87,5 +96,25 @@
     </section>
 
 </body>
+
+<script>
+    const curTime = document.getElementById("time");
+
+    function updateClock() {
+        const now = new Date();
+        const options = {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        };
+
+        curTime.innerHTML = now.toLocaleString('en-GB', options);
+    }
+
+    // Run it every 1000ms (1 second)
+    setInterval(updateClock, 1000);
+    updateClock(); // Run immediately so there's no 1-second delay
+</script>
 
 </html>
